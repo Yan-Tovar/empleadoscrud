d6c4e6a510d9430653001eb3fb54c3da3046aac7
Yan-Tovar <yantovar2007@gmail.com>
2025-09-01 17:28:35 -0500

    minuto 42

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "empleados_crud"
+})
+
+app.post("/create",(req,res) =>{
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const anios = req.body.anios;
+
+    db.query('INSERT INTO empleados(nombre, edad, pais, cargo, anios) Values(?,?,?,?,?)',[nombre, edad, pais, cargo, anios]),
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send("Empleado registrado con éxito")
+        }
+    }
+});
+
+app.listen(3001,()=>{
+    console.log("corriendo en el puerto 3001")
+}) 
